16efd6e756c074757a218a88556c7cde1e76bf71
Furiosa Volv <ilya.sh1711@icloud.com>
2022-07-08 11:24:40 +0300

    New edits

--- Occupy yourself/ViewController.swift
+++ Occupy yourself/ViewController.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Label ответственен за вывод строковых значений
     @IBOutlet weak var LabelOne: UILabel!
     
-    let List = ["Перестать прокрастинировать", "Подгтовить одежду на завтра", "Прочесть ту самую забытую книгу", "Полей цветы","А помнишь проект незаверщенный?", "Разамнись, деревящка..."]
+    // Arraу ответственен за хранение строковых значений
+    let List = ["Перестать прокрастинировать", "Подготовить одежду на завтра", "Прочесть ту самую забытую книгу", "Полей цветы","Помнишь о проекте незаверщенном?", "Разомнись, деревящка..."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+    // Button ответственен за интерактивность приложения
     @IBAction func ButtonOne(_ sender: Any) {
-        let index = Int(arc4random_uniform(UInt32(List.count)))
-        let result = List[index]
         
-        LabelOne.text = result
+        let index = Int(arc4random_uniform(UInt32(List.count))) // Строка ответственна за случайный выбор индекса из массива. Т.е индекс = строковое значение.
+        let result = List[index] //переменная для вывода случайного выбранного значения
+        
+        // Label ответственен за вывод строковых значений
+        LabelOne.text = result // вывод результата
     }
 }
 
